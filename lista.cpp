f627ccdb002bb880ef9fbe8e75b4cef9cb7aad51
Ibrahim <53277964+ibrixxx@users.noreply.github.com>
2020-12-28 22:51:11 +0100

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -1,96 +1,73 @@
-#ifndef LISTA_H
-#define LISTA_H
+#ifndef LISTA_CPP
+#define LISTA_CPP
+
+#include "lista.h"
 
 template <typename Tip>
-class Lista {
+void Lista<Tip>::init() {
+  glava = new Cvor(Tip());
+  rep = new Cvor(Tip(),glava);
+  glava->sljedeci = rep;
+  broj_elemenata = 0;
+}
 
-  struct Cvor {
-     Tip element;
-     Cvor *prethodni, *sljedeci;
-     Cvor(Tip element, Cvor *prethodni = nullptr, Cvor *sljedeci = nullptr)
-     :element(element),prethodni(prethodni), sljedeci(sljedeci) {}
-  };
+template <typename Tip>
+typename Lista<Tip>::Iterator Lista<Tip>::Insert(Iterator it, Tip vrijednost) {
+  it.pok->prethodni->sljedeci = new Cvor(vrijednost, it.pok->prethodni, it.pok);
+  it.pok->prethodni = it.pok->prethodni->sljedeci;
+  broj_elemenata++;
+  return Iterator(it.pok->prethodni);
+}
 
-  int broj_elemenata;
-  Cvor *glava, *rep;
+template <typename Tip>
+typename Lista<Tip>::Iterator Lista<Tip>::Erase(Iterator it) {
+  it.pok->prethodni->sljedeci = it.pok->sljedeci;
+  it.pok->sljedeci->prethodni = it.pok->prethodni;
+  Iterator za_vracanje(it.pok->sljedeci);
+  delete it.pok;
+  broj_elemenata--;
+  return za_vracanje;
+}
 
-  void init();
+template <typename Tip>
+Lista<Tip>::Lista(const Lista<Tip> &kopija) {
+  init();
+  for(auto it = kopija.Begin();it!=kopija.End();it++)
+    Push_Back(*it);
+}
 
-  public:
+template <typename Tip>
+Lista<Tip>::Lista(Lista<Tip> &&kopija):glava(kopija.glava),rep(kopija.rep),
+                                    broj_elemenata(kopija.broj_elemenata){
+  kopija.broj_elemenata = 0;
+  kopija.glava = nullptr;
+  kopija.rep = nullptr;
+}
 
-  class Const_Iterator {
-    protected:
-    Cvor *pok;
-    public:
-    Const_Iterator(Cvor *cvor = nullptr):pok(cvor) {}
-    Tip operator*() { return pok->element; }
-    Const_Iterator operator++() { pok = pok->sljedeci; return *this; }
-    Const_Iterator operator++(int) { Const_Iterator za_vracanje(*this); pok = pok->sljedeci; return za_vracanje; }
-    Const_Iterator operator--() { pok = pok->prethodni; return *this; }
-    Const_Iterator operator--(int) { Const_Iterator za_vracanje(*this); pok = pok->prethodni; return za_vracanje; }
-    bool friend operator==(Const_Iterator it1, Const_Iterator it2) { return it1.pok == it2.pok; }
-    bool friend operator!=(Const_Iterator it1, Const_Iterator it2) { return !(it1==it2); }
-    friend class Lista<Tip>;
-  };
+template <typename Tip>
+Lista<Tip>& Lista<Tip>::operator=(const Lista<Tip> &rhs) {
+  if(this == &rhs)
+    return *this;
 
-  class Iterator : public Const_Iterator {
-    public:
-    Iterator(Cvor *cvor = nullptr) { this->pok = cvor; }
-    Tip& operator*() { return this->pok->element; }
-    Iterator operator++() { this->pok = this->pok->sljedeci; return *this; }
-    Iterator operator++(int) { Iterator za_vracanje(*this); this->pok = this->pok->sljedeci; return za_vracanje; }
-    Iterator operator--() { this->pok = this->pok->prethodni; return *this; }
-    Iterator operator--(int) { Iterator za_vracanje(*this); this->pok = this->pok->prethodni; return za_vracanje; }
+  Clear();
+  for(auto it = rhs.Begin();it!=rhs.End();it++)
+    Push_Back(*it);
+  return *this;
+}
 
-  };
+template <typename Tip>
+Lista<Tip>& Lista<Tip>::operator=(Lista<Tip> &&rhs) {
+  swap(glava,rhs.glava);
+  swap(rep,rhs.rep);
+  broj_elemenata = rhs.broj_elemenata;
+  return *this;
+}
 
-  class Reverse_Iterator {
-    Cvor* pok;
-    public:
-    Reverse_Iterator(Cvor *cvor = nullptr):pok(cvor) {}
-    Tip operator*() { return pok->element; }
-    Reverse_Iterator operator--() { pok = pok->sljedeci; return *this; }
-    Reverse_Iterator operator--(int) { Reverse_Iterator za_vracanje(*this); pok = pok->sljedeci; return za_vracanje; }
-    Reverse_Iterator operator++() { pok = pok->prethodni; return *this; }
-    Reverse_Iterator operator++(int) { Reverse_Iterator za_vracanje(*this); pok = pok->prethodni; return za_vracanje; }
-    bool friend operator==(Reverse_Iterator it1, Reverse_Iterator it2) { return it1.pok == it2.pok; }
-    bool friend operator!=(Reverse_Iterator it1, Reverse_Iterator it2) { return !(it1==it2); }
-    friend class Lista<Tip>;
-  };
+template <typename tip>
+Lista<tip>& operator+(Lista<tip> &l1, const Lista<tip> &l2) {
+    for(auto it(l2.Begin()); it != l2.End(); it++)
+        l1.Push_Back(*it);
+    return l1;
+}
 
-  Lista() { init(); }
-  Lista(int n, Tip vrijednost) { init(); while(n-- > 0) Push_Back(vrijednost); }
-  Lista(const Lista<Tip> &kopija);
-  Lista<Tip>& operator=(const Lista<Tip> &rhs);
-  ~Lista() { Clear(); delete glava; delete rep; }
-  Lista(Lista<Tip> &&kopija);
-  Lista<Tip>& operator=(Lista<Tip> &&rhs);
-
-  Const_Iterator Begin() const { return glava->sljedeci; }
-  Const_Iterator End() const { return rep; }
-  Iterator Begin() { return glava->sljedeci; }
-  Iterator End() { return rep; }
-  Reverse_Iterator RBegin() {return rep->prethodni;}
-  Reverse_Iterator REnd() {return glava;}
-
-  Iterator Insert(Iterator pozicija, const Tip vrijednost);
-  void Push_Back(const Tip vrijednost) { Insert(End(),vrijednost); }
-  void Push_Front(const Tip vrijednost) { Insert(Begin(),vrijednost); }
-
-  Iterator Erase(Iterator pozicija);
-  void Pop_Back() { Erase(--End()); }
-  void PopFront() { Erase(Begin()); }
-
-  bool Empty() const { return broj_elemenata == 0; }
-  void Clear() { while(!Empty()) PopFront(); }
-  Lista<Tip>& operator+=(const Lista<Tip> &l) {*this = *this + l; return *this;}
-  Lista<Tip>& operator+=(Lista<Tip> &&rhs) {rep->prethodni->slijedeci = rhs.glava->slijedeci; rep = rhs.rep; broj_elemenata += rhs.broj_elemenata;
-  rhs.glava = nullptr; rhs.rep = nullptr; rhs.broj_elemenata = 0; return *this;}
-  template <typename tip>
-  friend Lista<tip>& operator+(Lista<tip> &l1, const Lista<tip> &l2);
-};
-
-#include "lista.cpp"
-
-
-#endif // LISTA_H
+#endif // LISTA_CPP
